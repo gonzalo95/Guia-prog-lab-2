2ec5271adfb78047a43cb90cca058308c59ab410
gonzalo <greco_gonzalo@hotmail.com>
2018-08-27 17:33:36 -0300

    Ejercicio 9

---
+++ año bisiesto/año bisiesto/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace año_bisiesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "EJercicio nº 6";
+
+            int num;
+            Console.Write("Ingrese el año: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num <= 0)
+            { 
+                Console.Write("Error, reingrese el año: ");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (num % 4 == 0 && (num % 100 != 0 || (num % 100 == 0 && num % 400 == 0)))
+                Console.Write("El año {0} es bisiesto", num);
+            else
+                Console.Write("El año {0} NO es bisiesto", num);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dias vividos/dias vividos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dias_vividos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ejercicio 7";
+
+            DateTime fechaDeNacimiento;
+            TimeSpan dias;
+            Console.Write("Ingrese su fecha de nacimiento(dd/mm/aaaa): ");
+            if(DateTime.TryParse(Console.ReadLine(), out fechaDeNacimiento))
+            {
+                dias = DateTime.Today - fechaDeNacimiento;
+            }
+            Console.WriteLine("Dias vividos: {0:dd}", dias);
+
+            Console.ReadKey();
+        }
+    }
+}
