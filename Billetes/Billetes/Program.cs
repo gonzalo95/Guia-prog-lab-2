64e7170de533aea3db54ba49949b070269047f13
gonzalo <greco_gonzalo@hotmail.com>
2018-09-04 17:46:37 -0300

    Testeo igualdad y desigualdad billetes

--- Billetes/Billetes/Program.cs
+++ Billetes/Billetes/Program.cs
@@ -10,6 +10,37 @@ namespace Billetes
   {
     static void Main(string[] args)
     {
+            Euro e = new Euro(1);
+            Dolar d = new Dolar(10);
+            Pesos p = new Pesos(100);
+
+
+            Console.WriteLine("---TESTS DE IGUALDAD Y DESIGUALDAD---");
+            Console.WriteLine(e != d);
+            Console.WriteLine(e == d);
+
+            Console.WriteLine(e != p);
+            Console.WriteLine(e == p);
+
+            Console.WriteLine(d != e);
+            Console.WriteLine(d == e);
+
+            Console.WriteLine(d != p);
+            Console.WriteLine(d == p);
+
+            Console.WriteLine(p != d);
+            Console.WriteLine(p == d);
+
+            Console.WriteLine(p != e);
+            Console.WriteLine(p == e);
+
+            Console.WriteLine(e == e);
+            Console.WriteLine(d == d);
+            Console.WriteLine(p == p);
+
+            Console.WriteLine("---TESTS DE IGUALDAD Y DESIGUALDAD CON CASTEO---");
+
+            Console.Read();
     }
   }
 }
