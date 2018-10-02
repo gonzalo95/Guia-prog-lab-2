c71a82c380073f7fc6760e102474687dc9fb7621
alumno <alumno>
2018-10-01 22:05:55 -0300

    Clases 10 y 11

---
+++ EjemploClasesAbstractas/EjemploClasesAbstractas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploClasesAbstractas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public abstract class Abstracta
+    {
+        public abstract int Metodo1(int a);
+
+        public virtual string Metodo1(int a, int b)
+        {
+            return "En abstracta";
+        }
+
+        public int Metodo1(string a)
+        {
+            return int.Parse(a);
+        }
+    }
+
+    public class Hija : Abstracta
+    {
+        public override int Metodo1(int a)
+        {
+            return a;
+        }
+    }
+
+    public override string Metodo1(int a, int b)
+    {
+        return "En hija";
+    }
+}
---
+++ Final_Clase_11/Clase_10_Library/Inversionista.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_8_Library
+{
+    public class Inversionista : Persona
+    {
+        private int _porcionAccionaria;
+
+        public Inversionista(string nombre, string apellido, int porcion) : base(nombre, apellido)
+        {
+            PorcionAccionaria = porcion;
+        }
+
+        public int PorcionAccionaria
+        {
+            set
+            {
+                if (value <= 100 && value > 0) this._porcionAccionaria = value;
+            }
+            get
+            {
+                return this._porcionAccionaria;
+            }
+        }
+
+        public override string PosicionSocietaria()
+        {
+            return "Accionista con el " + this.PorcionAccionaria + "% de la porción accionaria";
+        }
+
+        public static bool operator ==(Inversionista i1, Inversionista i2)
+        {
+            return i1.Apellido == i2.Apellido && i1.Nombre == i2.Nombre && i1.PorcionAccionaria == i2.PorcionAccionaria;
+        }
+
+        public static bool operator !=(Inversionista i1, Inversionista i2)
+        {
+            return !(i1 == i2);
+        }
+
+        private string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Nombre: " + base.Nombre);
+            sb.AppendLine("Apellido: " + base.Apellido);
+            sb.AppendLine("Procion societaria: " + this.PorcionAccionaria);
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+    }
+}
