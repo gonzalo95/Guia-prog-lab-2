d35804305512e58b45f37bb367fc8b56d8fbe7b9
gonzalo <greco_gonzalo@hotmail.com>
2018-10-17 17:33:39 -0300

    Ejercico 40: solo falta formulario

---
+++ CentralAbstracta/Entidades/Llamada.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Llamada
+    {
+        public enum TipoLlamada
+        {
+            Local,
+            Provincial,
+            Todas
+        }
+
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+
+        public abstract float CostoLlamada { get;  }
+
+        public float Duracion
+        {
+            get
+            {
+                return this.duracion;
+            }
+        }
+
+        public string NroDestino
+        {
+            get
+            {
+                return this.nroDestino;
+            }
+        }
+
+        public string NroOrigen
+        {
+            get
+            {
+                return this.nroOrigen;
+            }
+        }
+
+        public Llamada(float duracion, string destino, string origen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = destino;
+            this.nroOrigen = origen;
+        }
+
+        protected virtual string Mostrar()
+        {
+            StringBuilder salida = new StringBuilder();
+
+            salida.AppendFormat("Duracion: {0}\n", Duracion);
+            salida.AppendFormat("Destino: {0}\n", NroDestino);
+            salida.AppendFormat("Origen: {0}\n", NroOrigen);
+
+            return salida.ToString();
+        }
+
+        public static int OrdenarPorDuracion(Llamada l1, Llamada l2)
+        {
+            int salida = 0;
+
+            if (l1.Duracion < l2.Duracion)
+                salida = -1;
+
+            else if (l1.Duracion > l2.Duracion)
+                salida = 1;
+
+            return salida;
+        }
+
+        public static bool operator ==(Llamada l1, Llamada l2)
+        {
+            return l1.Equals(l2);
+        }
+
+        public static bool operator !=(Llamada l1, Llamada l2)
+        {
+            return !(l1 == l2);
+        }
+    }
+}
---
+++ CentralAbstracta/Entidades/Provincial.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Provincial : Llamada
+    {
+        public enum Franja
+        {
+            Franja_1,
+            Franja_2,
+            Franja_3
+        }
+
+        protected Franja franjaHoraria;
+
+        public override float CostoLlamada
+        {
+            get
+            {
+                return CalcularCosto();
+            }
+        }
+        public Provincial(string origen, Franja miFranja, float duracion, string destino)
+            : base(duracion, destino, origen)
+        {
+            this.franjaHoraria = miFranja;
+        }
+
+        public Provincial(Franja miFranja, Llamada llamada)
+            : this(llamada.NroOrigen, miFranja, llamada.Duracion, llamada.NroDestino)
+        {
+        }
+
+        protected override string Mostrar()
+        {
+            StringBuilder salida = new StringBuilder();
+
+            salida.Append(base.Mostrar());
+            salida.AppendFormat("Franja: {0}\n", this.franjaHoraria);
+            salida.AppendFormat("Costo: {0}\n", CostoLlamada);
+            salida.AppendLine("-----------");
+
+            return salida.ToString();
+        }
+
+        private float CalcularCosto()
+        {
+            float costo = 0;
+
+            switch (this.franjaHoraria)
+            {
+                case Franja.Franja_1:
+                    costo = this.Duracion * (float)1.99;
+                    break;
+
+                case Franja.Franja_2:
+                    costo = this.Duracion * (float)2.25;
+                    break;
+
+                case Franja.Franja_3:
+                    costo = this.Duracion * (float)1.66;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return costo;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == typeof(Provincial);
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+    }
+}
